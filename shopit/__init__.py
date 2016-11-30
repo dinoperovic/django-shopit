1fd76412b1d5586069bb34cd113a029a0d2d6fa1
Dino Perovic <dino.perovic@gmail.com>
2016-11-30 12:38:47 +0100

    Bump to 0.1.1

--- shopit/__init__.py
+++ shopit/__init__.py
@@ -8,12 +8,13 @@ Release logic:
  3. git commit -m 'Bump to <version>'
  4. git tag v<version>
  5. git push && git push --tags
- 6. python setup.py sdist upload
- 7. bump the __version__, append ".dev0"
- 8. git add shopit/__init__.py
- 9. git commit -m 'Start with <version>'
-10. git push
+ 6. python setup.py sdist
+ 7. twine upload dist/djangoshop-shopit-<version>.tar.gz
+ 8. bump the __version__, append ".dev0"
+ 9. git add shopit/__init__.py
+10. git commit -m 'Start with <version>'
+11. git push
 """
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 default_app_config = 'shopit.apps.ShopitConfig'
