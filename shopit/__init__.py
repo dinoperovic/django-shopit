2688b2daad65f2aea61d28ed7d0978d4c43cca13
Dino Perovic <dino.perovic@gmail.com>
2017-04-12 09:41:09 +0200

    Bump to 0.2.0

--- shopit/__init__.py
+++ shopit/__init__.py
@@ -14,6 +14,6 @@ Release logic:
  9. twine upload dist/djangoshop-shopit-<version>.tar.gz
 10. Done!
 """
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 
 default_app_config = 'shopit.apps.ShopitConfig'
