e123d488792fd067c6f1c84d6a0f785dfa4ac42b
Dino Perovic <dino.perovic@gmail.com>
2017-04-20 13:34:18 +0200

    update init

--- shopit/__init__.py
+++ shopit/__init__.py
@@ -8,7 +8,7 @@ Release logic:
  3. git commit -m 'Bump to <version>'
  4. git push
  5. Make sure all tests pass on https://travis-ci.org/dinoperovic/djangoshop-shopit
- 6. git tag v<version>
+ 6. git tag <version>
  7. git push --tags
  8. python setup.py sdist
  9. twine upload dist/djangoshop-shopit-<version>.tar.gz
